bf943e695003fb4ebcec1a868ae74c5afa05898c
Raphaël Lefèvre <raphael.lefevre1@gmail.com>
2020-08-28 16:29:33 +0200

    Change name convention

--- operational-research/a_star_example.hpp
+++ operational-research/a_star_example.hpp
@@ -1,7 +1,7 @@
 #ifndef A_STAR_TEST_H
 #define A_STAR_TEST_H
 
-#include <Merlin-operational-research/merlin_a_star.hpp>
+#include <merlin-operational-research/merlin_a_star.hpp>
 
 #include <vector>
 using simple_map_t = std::vector<std::vector<short>>;
